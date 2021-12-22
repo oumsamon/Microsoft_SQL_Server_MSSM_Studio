6fe6d6e81fddaceb588d8892802401741117cd6b
oumsamon <oumsamon@gmail.com>
2021-12-22 16:44:38 -0600

    view with 2 tables

--- views.sql
+++ views.sql
@@ -10,3 +10,20 @@ If object_id(N' schema_name, sysname, dbo>.<view_name, sysname, Top10Sales>', 'V
 GO
 
 CREATE VIEW <schema_name, sysname, dbo>.<view_name, sysname, Top10Sales> AS <select_statement, , SELECT TOP 10 * FROM Sales.SalesOrderHeader ORDER BY TotalDue DESC>
+
+
+--- VIEWS (US VIEW)
+
+CREATE VIEW MyCustomUSView
+AS
+SELECT * FROM dogs
+WHERE color LIKE 'red'
+
+SELECT * FROM MyCustomUSView
+
+--- joining 2 tables and creating that VIEW
+
+CREATE VIEW NASalesQuota
+SELECT [Name], [Group]. [SalesQuota], [Bonus]
+FROM dogs A INNER JOIN cats B
+ON 
