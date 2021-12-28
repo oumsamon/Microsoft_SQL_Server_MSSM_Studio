153fbf5d1b36577fd2eae0570c4014147817c895
oumsamon <oumsamon@gmail.com>
2021-12-28 15:24:23 -0600

    Try and Catch

--- transactionsAndErrorHandling.sql
+++ transactionsAndErrorHandling.sql
@@ -1,2 +1,10 @@
 ---ACID when building/managing DBs---
 ---Atomicity, Consistency, Isolation and Durability in CS Term--
+
+--- Committ Transactions or Rollback---
+--Try and Catch(Error handling code) 
+
+
+
+
+
