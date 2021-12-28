8205ece97eae823e36641f82dcf5d9a634854c1c
oumsamon <oumsamon@gmail.com>
2021-12-28 14:44:50 -0600

    basic UDF

--- userDefineFunc.sql
+++ userDefineFunc.sql
@@ -1,4 +1,25 @@
----UDF is similar to StorProc---
+---UDF is similar but different to StorProc---
+
+---Scalar Function(single data value), Table-Value Functions, System Functions(cannot not be modified)
+---Syst Func msdn.mircrosoft.com/en-us/library/ms191007.aspx
+
+SELECT * FROM tableName
+
+CREATE FUNCTION YTDSALES()
+RETURN MONEY
+AS
+BEGIN
+DECLARE @YTDSALES MONEY
+SELECT @YTDSALES = SUM(colName) FROM tableName
+RETURN @YTDSALES
+END
+
+DECLARE @YTDRESULT  AS MONEY
+SELECT @YTDRESULT = dbo.YTDSALES()
+PRINT @YTDRESULT
+
+--PARAMETERIZED FUNCTIONS
+
 
 
 
